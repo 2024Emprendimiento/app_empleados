69a73c00eb635c74011aa83949a0569bb325d723
2024Emprendimiento <wayuunuti@gmail.com>
2025-07-21 10:41:59 -0500

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); 
-const URI = 'mongodb+srv://root:1234@cluster0.6fc7p1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const URI = 'mongodb+srv://root:...';
 mongoose.connect(URI)
      .then(db => console.log('DB is connected'))
      .catch(err => console.error(err));  
